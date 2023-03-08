e686eba3a06f35a31e1b770d9c68a4f7168f01a6
Oscar Karbin <karbin.oscar2002@gmail.com>
2023-03-08 19:14:22 +0000

    build

--- app/src/__tests__/api/app.test.js
+++ app/src/__tests__/api/app.test.js
@@ -1,6 +1,7 @@
 const app = require("../../server/app");
 const supertest = require("supertest");
 
+
 test("GET /ping", async () => {
   await supertest(app).get("/ping")
       .expect(200)
@@ -8,4 +9,74 @@ test("GET /ping", async () => {
         // Check data
         expect(response.body.result).toBe("pong");
       });
+});
+
+describe('POST /presentations', () => {
+  it('should return a 400 status code when creating a presentation with no polls', async () => {
+    const response = await supertest(app)
+      .post('/presentations')
+      .send({});
+    expect(response.statusCode).toBe(400);
+  });
+});
+
+describe('GET /presentations/:presentation_id/polls', () => {
+  it('should return a 404 status code when requesting a non-existent presentation', async () => {
+    const response = await supertest(app)
+      .get('/presentations/unknown_presentation_id/polls')
+      .set('Content-Type', 'application/json');
+    expect(response.statusCode).toBe(404);
+  });
+});
+
+describe('Interactive Presentation API', () => {
+  let presentationId, pollId;
+
+  test('creating a presentation and showing a poll', async () => {
+    const res1 = await supertest(app)
+      .post('/presentations')
+      .send({
+        polls: [
+          {
+            question: "What's your favorite pet?",
+            options: [
+              {key: "A", value: "Dog"},
+              {key: "B", value: "Cat"},
+              {key: "C", value: "Crocodile"}
+            ]
+          },
+          {
+            question: "Which of the countries would you like to visit the most?",
+            options: [
+              {key: "A", value: "Argentina"},
+              {key: "B", value: "Austria"},
+              {key: "C", value: "Australia"}
+            ]
+          }
+        ]
+      })
+
+      console.log(res1.body.presentation_id)
+    expect(res1.body.presentation_id).toBeDefined();
+    presentationId = res1.body.presentation_id;
+    
+  
+
+     const res2 = await supertest(app)
+      .put(`/presentations/${presentationId}/polls/current`)
+      .expect(200);
+
+    expect(res2.body.poll_id).toBeDefined();
+    expect(res2.body.question).toBe("Which of the countries would you like to visit the most?");
+    expect(res2.body.votes).toBeUndefined();
+    pollId = res2.body.poll_id; 
+
+     const res3 = await supertest(app)
+      .get(`/presentations/${presentationId}/polls/current`)
+      .expect(200);
+
+    expect(res3.body.poll_id).toBeDefined();
+    expect(res3.body.question).toBe("Which of the countries would you like to visit the most?");
+    expect(res3.body.votes).toBeUndefined(); 
+  });
 });
--- app/src/api/index.js
+++ app/src/api/index.js
@@ -1,5 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+const { options } = require("../server/app");
+
+class Poll {
+  constructor({ question, options }) {
+    this.id = uuidv4(); // Generate a new unique id for each poll
+    this.question = question;
+    this.options = options;
+  }
+}
+
+class Presentation {
+  constructor({polls}) {
+    this.id = uuidv4(); // Generate a new unique id for the presentation
+    this.polls = polls.map(poll => new Poll(poll));
+    this.currentPollIndex = 0;
+  }
+
+  getCurrentPoll() {
+    return this.polls[this.currentPollIndex];
+  }
+
+  getNextPoll() {
+    this.currentPollIndex++;
+    return this.getCurrentPoll();
+  }
+}
 
 router.get("/ping", (req, res) => {
   res.send({
@@ -7,5 +34,56 @@ router.get("/ping", (req, res) => {
   })
 })
 
+let presentations = []
+
+router.post('/presentations', (req, res) =>{
+  var polls = req.body.polls;
+  if (!req.body || !req.body.polls || req.body.polls.length === 0) {
+    res.status(400).send('Invalid request body');
+    return;
+  }
+  let pres = new Presentation({polls});
+  presentations.push(pres);
+  console.log(pres.id)
+
+  res.status(200).json({
+    presentation_id: pres.id 
+  })
+});
+
+router.put('/presentations/:presentation_id/polls/current', (req, res) => {
+
+  var presentationId = req.params.presentation_id;
+  var presentation = presentations.find(p => p.id === presentationId);
+  console.log(presentation)
+
+  if (!presentation) {
+    res.status(404).send('Presentation not found');
+    return;
+  }
+
+  var new_current_poll = presentation.getNextPoll();
+
+  res.status(200).json({
+    poll_id: new_current_poll.id,
+    question: new_current_poll.question,
+    options: new_current_poll.options
+  });
+});
+
+
+router.get('/presentations/:presentation_id/polls/current', (req, res) =>{
+  
+  var presentationId = req.params.presentation_id;
+  var presentation = presentations.find(p => p.id === presentationId);
+  var current_poll = presentation.getCurrentPoll();
+
+  res.status(200).json({
+    poll_id: current_poll.id,
+    question: current_poll.question,
+    options: current_poll.options
+  });
+
+});
 
 module.exports = router;
